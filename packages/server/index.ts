8748a8b3379388328a12a066480f2789756d93dc
Ranjith Kumar R G <theranjith@gmail.com>
2025-09-11 23:16:11 +0530

    input validation and added conversationid

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import OpenAI from 'openai';
+import z from 'zod';
 
 dotenv.config();
 
@@ -22,15 +23,34 @@ app.get('/api/hello', (req: Request, res: Response) => {
    res.json({ message: 'Hello World!' });
 });
 
+const conversations = new Map<string, string>();
+
+const chatSchema = z.object({
+   prompt: z
+      .string()
+      .trim()
+      .min(1, 'Prompt is required')
+      .max(1000, 'Prompt is too long'),
+   conversationId: z.uuid(),
+});
+
 app.post('/api/chat', async (req: Request, res: Response) => {
-   const { prompt } = req.body;
+   const parsedResult = chatSchema.safeParse(req.body);
+   if (!parsedResult.success) {
+      return res.status(400).json(parsedResult.error.format);
+   }
+
+   const { prompt, conversationId } = req.body;
 
    const response = await client.responses.create({
       model: 'gpt-4o-mini',
       input: prompt,
       temperature: 0.2,
       max_output_tokens: 100,
+      previous_response_id: conversations.get(conversationId),
    });
+
+   conversations.set(conversationId, response.id);
 
    res.json({ message: response.output_text });
 });
