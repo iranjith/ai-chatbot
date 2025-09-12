603a4f0b00ac7793a3f94b67fdc09d1c165c024a
Ranjith Kumar R G <theranjith@gmail.com>
2025-09-12 10:38:09 +0530

    repository patter - added service and repository

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,14 +1,10 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import OpenAI from 'openai';
 import z from 'zod';
+import { chatService } from './services/chat.service';
 
 dotenv.config();
-
-const client = new OpenAI({
-   apiKey: process.env.OPENAI_API_KEY,
-});
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -22,8 +18,6 @@ app.get('/', (req: Request, res: Response) => {
 app.get('/api/hello', (req: Request, res: Response) => {
    res.json({ message: 'Hello World!' });
 });
-
-const conversations = new Map<string, string>();
 
 const chatSchema = z.object({
    prompt: z
@@ -42,17 +36,9 @@ app.post('/api/chat', async (req: Request, res: Response) => {
 
    try {
       const { prompt, conversationId } = req.body;
-      const response = await client.responses.create({
-         model: 'gpt-4o-mini~',
-         input: prompt,
-         temperature: 0.2,
-         max_output_tokens: 100,
-         previous_response_id: conversations.get(conversationId),
-      });
 
-      conversations.set(conversationId, response.id);
-
-      res.json({ message: response.output_text });
+      const response = await chatService.sendMessage(prompt, conversationId);
+      res.json({ message: response.message });
    } catch (error) {
       res.status(500).json({
          error: 'An error occurred while processing your request.',
---
+++ packages/server/repositories/conversation.repository.ts
@@ -0,0 +1,15 @@
+//Implementation detail
+const conversations = new Map<string, string>();
+
+export const conversationRepository = {
+   getLastResponseId,
+   setLastResponseId,
+};
+
+function getLastResponseId(conversationId: string): string | undefined {
+   return conversations.get(conversationId);
+}
+
+function setLastResponseId(conversationId: string, responseId: string): void {
+   conversations.set(conversationId, responseId);
+}
