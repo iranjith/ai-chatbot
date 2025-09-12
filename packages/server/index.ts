22ed87e466918e5eb9ee4255a8a7dacbd1befd6b
Ranjith Kumar R G <theranjith@gmail.com>
2025-09-12 11:28:59 +0530

    refactor: added routes

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,24 +1,14 @@
 import dotenv from 'dotenv';
-import type { Request, Response } from 'express';
 import express from 'express';
-import { chatController } from './controllers/chat.controller';
+import router from './routes/route';
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
-
 app.use(express.json());
+app.use(router);
 
-app.get('/', (req: Request, res: Response) => {
-   res.send('Hello World!');
-});
-
-app.get('/api/hello', (req: Request, res: Response) => {
-   res.json({ message: 'Hello World!' });
-});
-
-app.post('/api/chat', chatController.sendMessage);
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
    console.log(`Server is running on http://localhost:${port}`);
